0b2784cd3d0ae263de24ea8b489e0e76a4fad8d7
Rômulo <romuloaraujofonseca@gmail.com>
2024-08-30 13:01:20 -0300

    Primeiro commit

---
+++ Teste4Script.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000); // Muda a imagem a cada 4 segundos
+}
+
+/* Locais */
+document.querySelectorAll('.favorite-icon').forEach(function(icon) {
+icon.addEventListener('click', function() {
+icon.classList.toggle('active');
+});
+});
